0002dd2f24271487ec08fae0e134c16c2bc2a591
Solo <b01bc@mail.ru>
2023-05-28 02:12:22 +0300

    Analysis update

--- src/main/java/com/project/vetfacade/bisentity/AnalysisEntity.java
+++ src/main/java/com/project/vetfacade/bisentity/AnalysisEntity.java
@@ -1,34 +1,54 @@
 package com.project.vetfacade.bisentity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.vetfacade.dto.VisitDTO;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-//TODO найти инфомрацию по анализам в БД и реализацивать
 //тут пока заглушка
 public class AnalysisEntity {
-    private Long analysis_id;
-    private String doctor_name;
-    private boolean ready;
-    private String analysis_name;
-    private Long visit_id;
+
+    @JsonProperty("analysis_id")
+    private Long analysisId;
+
+
+    private PetLightEntity pet;
+
+
+    private VisitEntity visit;
+
+
+    @JsonProperty("analysis_name")
+    private String analysisName;
+
+
+    @JsonProperty("is_ready")
+    private boolean isReady;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private LocalDateTime date;
     public static AnalysisEntity toEntity() {
         AnalysisEntity entity = new AnalysisEntity();
-        entity.setAnalysis_id(1L);
-        entity.setDoctor_name("Мстислав");
-        entity.setReady(false);
-        entity.setAnalysis_name("Прививка от бешенства");
-        entity.setVisit_id(14L);
+        PetLightEntity pet = new PetLightEntity();
+        pet.setName("Кеша");
+        entity.setAnalysisId(1L);
+        entity.setPet(pet);
+        VisitDTO visit = new VisitDTO();
+        visit.setVisit_id(130L);
+        entity.setVisit(VisitEntity.toEntity(visit));
+        entity.setAnalysisName("Анализ крови");
+        entity.setDate(LocalDateTime.of(2023, 1, 12, 1, 30));
+        entity.setReady(true);
         return entity;
     }
-    public AnalysisEntity(){
-        this.setAnalysis_id(1L);
-        this.setDoctor_name("Мстислав");
-        this.setReady(false);
-        this.setAnalysis_name("Прививка от бешенства");
-        this.setVisit_id(14L);
-    }
+
 }

735e8530436b5d7ab1f55c75521cab5283de53bc
kurama <vivadan02@gmail.com>
2023-05-31 21:08:07 +0300

    get_surgeons

--- src/main/java/com/project/vetfacade/controller/ManipulationController.java
+++ src/main/java/com/project/vetfacade/controller/ManipulationController.java
@@ -1,10 +1,24 @@
 package com.project.vetfacade.controller;
 
+import com.project.vetfacade.dto.ManipulationDTO;
+import com.project.vetfacade.service.ManipulationService;
+import jakarta.annotation.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/v3")
-public class ManipulationController {
+import java.util.List;
 
+@RestController
+@RequestMapping("/api/v1")
+public class ManipulationController {
+    @Autowired
+    private ManipulationService manipulationService;
+    @GetMapping("/get_surgeons")
+    public ResponseEntity<List<ManipulationDTO>> getSurgeons(@Nullable @RequestParam String name) {
+        return ResponseEntity.ok(manipulationService.getManipulations(name));
+    }
 }
